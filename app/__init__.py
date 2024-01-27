a63f2dc4173da614ac2672096da75635187a4848
Emily Wang <ewang0332@gmail.com>
2024-01-27 14:54:32 -0500

    added very basic create group page with POST request

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.declarative import declared_attr
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
+from flask import render_template, request
 from app import app
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return "Hello world"
+def home():
+    return render_template('home.html')
+
+@app.route('/create_group', methods=['GET', 'POST'])
+def create_group():
+    if request.method == "POST":
+        creator_name = request.form["creator_name"]
+        return creator_name
+    return render_template('create_group.html')
