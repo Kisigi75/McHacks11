53f5a0e66abad1989f7a170b492c8b5d411bf5ad
Emily Wang <ewang0332@gmail.com>
2024-01-27 18:57:23 -0500

    changing direction to no group feature, reformatted pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 app.secret_key = "super secret key"
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -4,38 +4,19 @@ import string
 import random
 
 @app.route('/')
+@app.route('/index')
 def home():
     return render_template('home.html')
 
-@app.route('/create_group', methods=['GET', 'POST'])
-def create_group():
-    if request.method == "POST":
-        creator_name = request.form["creator_name"]
-        session["creator_name"] = creator_name
-        # generate random code
-        random_code = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
-        session["random_code"] = random_code
-
-        return redirect(url_for("dashboard"))
-    
-    return render_template('create_group.html')
-
-@app.route('/join_group', methods=['GET', 'POST'])
-def join_group():
-    if request.method == "POST":
-        user_name = request.form["user_name"]
-        entered_code = request.form["entered_code"]
-        session["user_name"] = user_name
-        session["entered_code"] = entered_code
-        return redirect(url_for('dashboard'))
-
-    return render_template('join_group.html')
-
-
 @app.route('/dashboard')
 def dashboard():
-    if "creator_name" in session:
-        creator_name = session["creator_name"]
-        random_code = session["random_code"]
+    return render_template('dashboard.html')
 
-    return render_template("dashboard.html", creator_name=creator_name, random_code=random_code)
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/create_account')
+def create_account():
+    return render_template('create_account.html')
+
